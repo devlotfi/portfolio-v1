fee78a51f845caae31e29722c4fb85884d0c0da0
Debbal Lotfi <debbal.lotfi.dev@gmail.com>
2024-07-20 23:50:03 +0100

    update

--- client/src/components/project/project.component.tsx
+++ client/src/components/project/project.component.tsx
@@ -2,14 +2,6 @@ import { BaseHTMLAttributes, useContext, useRef } from "react";
 import { cn } from "../../utils/cn";
 import { components } from "../../__generated__/schema";
 import { ProjectListContext } from "../../context/project-list.context";
-import ProjectReadme from "./project-readme.component";
-import Button from "../button/button.component";
-import ButtonContent from "../button/button-content.component";
-import ButtonSlider from "../button/button-slider.component";
-import ButtonIcon from "../button/button-icon.component";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faBackward } from "@fortawesome/free-solid-svg-icons";
-import ButtonText from "../button/button-text.component";
 import Heading from "../heading/heading.component";
 import HeadingText from "../heading/heading-text.component";
 
@@ -34,9 +26,6 @@ export default function Project({ className, project, ...props }: Props) {
       onClick={handleCardClick}
       className={cn(
         "flex w-full h-[25rem] md:w-[48%] mb-[1rem] border bg-edge-100 border-edge-100 duration-300",
-        selectedProject?.id === project.id &&
-          "!w-full h-auto snap-center scroll-mt-[3.8rem] lg:scroll-mt-0 order-first",
-        selectedProject && selectedProject.id !== project.id && "hidden",
         className
       )}
       {...props}
@@ -49,20 +38,13 @@ export default function Project({ className, project, ...props }: Props) {
         )}
       >
         <div
-          className={cn(
-            "flex bg-base-100 justify-between items-center p-[0.7rem]",
-            selectedProject?.id === project.id &&
-              "sticky top-0 border-b border-edge-100"
-          )}
+          className={"flex bg-base-100 justify-between items-center p-[0.7rem]"}
         >
           <div className="flex space-x-3 justify-start">
             <div className="flex border border-edge-100">
               <div className="flex border border-edge-100 bg-base-100 p-[0.5rem] translate-x-[0.3rem] translate-y-[-0.3em]">
                 <img
-                  className={cn(
-                    "h-[3.5rem] w-[3.5rem]",
-                    selectedProject?.id === project.id && "h-[2rem] w-[2rem]"
-                  )}
+                  className={"h-[3.5rem] w-[3.5rem]"}
                   src={project.icon}
                   alt="icon"
                 />
@@ -75,38 +57,21 @@ export default function Project({ className, project, ...props }: Props) {
                   {project.name}
                 </HeadingText>
               </Heading>
-              {selectedProject?.id !== project.id ? (
-                <div className="flex text-[10pt] opacity-70">
-                  {project.description}
-                </div>
-              ) : null}
+
+              <div className="flex text-[10pt] opacity-70">
+                {project.description}
+              </div>
             </div>
           </div>
-
-          {selectedProject?.id === project.id ? (
-            <Button inverted onClick={() => setSelectedProject(null)}>
-              <ButtonContent>
-                <ButtonIcon>
-                  <FontAwesomeIcon icon={faBackward}></FontAwesomeIcon>
-                </ButtonIcon>
-                <ButtonText>Back</ButtonText>
-                <ButtonSlider></ButtonSlider>
-              </ButtonContent>
-            </Button>
-          ) : null}
         </div>
 
-        {selectedProject?.id === project.id ? (
-          <ProjectReadme project={project}></ProjectReadme>
-        ) : (
-          <div className="flex p-[0.7rem] overflow-hidden w-full h-full">
-            <img
-              className="h-full w-full overflow-hidden border border-edge-100 object-cover object-top"
-              src={project.thumbnail}
-              alt="thumbnail"
-            />
-          </div>
-        )}
+        <div className="flex p-[0.7rem] overflow-hidden w-full h-full">
+          <img
+            className="h-full w-full overflow-hidden border border-edge-100 object-cover object-top"
+            src={project.thumbnail}
+            alt="thumbnail"
+          />
+        </div>
       </div>
     </div>
   );
--- client/src/views/projects.component.tsx
+++ client/src/views/projects.component.tsx
@@ -6,17 +6,23 @@ import Heading from "../components/heading/heading.component";
 import HeadingIcon from "../components/heading/heading-icon.component";
 import HeadingText from "../components/heading/heading-text.component";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faListCheck } from "@fortawesome/free-solid-svg-icons";
+import { faBackward, faListCheck } from "@fortawesome/free-solid-svg-icons";
 import ProjectSearch from "../components/project/project-search.component";
 import Project from "../components/project/project.component";
 import { cn } from "../utils/cn";
 import { useContext, useEffect, useRef } from "react";
 import { ProjectListContext } from "../context/project-list.context";
 import ProjectPagination from "../components/project/project-pagination.component";
+import ProjectReadme from "../components/project/project-readme.component";
+import ButtonContent from "../components/button/button-content.component";
+import ButtonIcon from "../components/button/button-icon.component";
+import ButtonSlider from "../components/button/button-slider.component";
+import ButtonText from "../components/button/button-text.component";
+import Button from "../components/button/button.component";
 
 export default function ProjectsView() {
   const listRef = useRef<HTMLDivElement>(null);
-  const { selectedProject, search, categories, page } =
+  const { selectedProject, setSelectedProject, search, categories, page } =
     useContext(ProjectListContext);
 
   const { data: dataCategories, isLoading: isLoadingCategories } = useQuery({
@@ -40,17 +46,56 @@ export default function ProjectsView() {
 
   useEffect(() => {
     listRef.current?.scrollTo({ top: 0, behavior: "smooth" });
-  }, [selectedProject, page]);
+  }, [page]);
 
   return (
     <ViewContent
       reactRef={listRef}
-      className={cn("items-center relative overflow-x-hidden")}
+      className="items-center relative overflow-x-hidden"
     >
-      <PageLoading loading={isLoadingCategories}>
-        <div className="flex flex-col w-full p-[1rem] space-y-7 flex-1 lg:max-w-screen-lg">
-          {!selectedProject ? (
-            <>
+      {selectedProject ? (
+        <div className="w-full animate-[fadeInRight_0.7s_ease] fill-mode-backward">
+          <div className="flex w-full bg-base-100 justify-between items-center p-[0.7rem] sticky top-0 border-b border-edge-100">
+            <div className="flex flex-col sm:flex-row items-start sm:items-start sm:space-x-3 sm:justify-start">
+              <div className="flex border border-edge-100">
+                <div className="flex border border-edge-100 bg-base-100 p-[0.5rem] translate-x-[0.3rem] translate-y-[-0.3em]">
+                  <img
+                    className={cn(
+                      "h-[3.5rem] w-[3.5rem]",
+                      selectedProject?.id === selectedProject.id &&
+                        "h-[2rem] w-[2rem]"
+                    )}
+                    src={selectedProject.icon}
+                    alt="icon"
+                  />
+                </div>
+              </div>
+
+              <div className="flex flex-col text-edge-100">
+                <Heading>
+                  <HeadingText className="pl-[0.3rem]">
+                    {selectedProject.name}
+                  </HeadingText>
+                </Heading>
+              </div>
+            </div>
+
+            <Button inverted onClick={() => setSelectedProject(null)}>
+              <ButtonContent>
+                <ButtonIcon>
+                  <FontAwesomeIcon icon={faBackward}></FontAwesomeIcon>
+                </ButtonIcon>
+                <ButtonText>Back</ButtonText>
+                <ButtonSlider></ButtonSlider>
+              </ButtonContent>
+            </Button>
+          </div>
+          <ProjectReadme project={selectedProject}></ProjectReadme>
+        </div>
+      ) : (
+        <>
+          <PageLoading loading={isLoadingCategories}>
+            <div className="flex flex-col w-full p-[1rem] space-y-7 flex-1 lg:max-w-screen-lg">
               <Heading className="text-[20pt]" variant="primary">
                 <HeadingIcon>
                   <FontAwesomeIcon icon={faListCheck}></FontAwesomeIcon>
@@ -58,24 +103,24 @@ export default function ProjectsView() {
                 <HeadingText>Projects</HeadingText>
               </Heading>
               <ProjectSearch categories={dataCategories!}></ProjectSearch>
-            </>
-          ) : null}
 
-          <PageLoading loading={isLoadingProjects}>
-            <div className={cn("flex flex-wrap justify-between")}>
-              {dataProjects?.results.map((project) => {
-                return <Project key={project.id} project={project}></Project>;
-              })}
+              <PageLoading loading={isLoadingProjects}>
+                <div className={"flex flex-wrap justify-between"}>
+                  {dataProjects?.results.map((project) => {
+                    return (
+                      <Project key={project.id} project={project}></Project>
+                    );
+                  })}
+                </div>
+
+                <ProjectPagination
+                  count={dataProjects?.count as number}
+                ></ProjectPagination>
+              </PageLoading>
             </div>
-
-            {!selectedProject ? (
-              <ProjectPagination
-                count={dataProjects?.count as number}
-              ></ProjectPagination>
-            ) : null}
           </PageLoading>
-        </div>
-      </PageLoading>
+        </>
+      )}
     </ViewContent>
   );
 }
