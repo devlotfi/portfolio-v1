288d61caac6b4074b84c3437d834a8327225b5c3
Debbal Lotfi <debbal.lotfi.dev@gmail.com>
2024-07-26 21:58:45 +0100

    ui fix

--- client/src/components/project/project-readme.component.tsx
+++ client/src/components/project/project-readme.component.tsx
@@ -21,7 +21,7 @@ export default function ProjectReadme({ className, project, ...props }: Props) {
 
   return (
     <PageLoading loading={isLoading}>
-      <div className="flex p-[1.5rem] w-full max-w-[980px]">
+      <div className="flex overflow-x-auto p-[1.5rem] w-full max-w-[980px]">
         <div
           dangerouslySetInnerHTML={{
             __html: DOMPurify.sanitize(data!),
