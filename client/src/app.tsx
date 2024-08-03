e0b2f63b51cb88da5ce3039d22f442bfb7418e5c
Debbal Lotfi <debbal.lotfi.dev@gmail.com>
2024-08-03 19:07:36 +0100

    added background effect

--- client/src/app.tsx
+++ client/src/app.tsx
@@ -1,42 +1,42 @@
-import { useContext, useEffect, useState } from "react";
-import { NavigationContext, Views } from "./context/navigation.context";
-import TransitionLoading from "./components/transition-loading/transition-loading.component";
-import { EmptyView } from "./layout/empty-view.component";
-import { cn } from "./utils/cn";
-import ContactView from "./views/contact.component";
-import AboutView from "./views/home.component";
-import ProjectsView from "./views/projects.component";
-import SkillsView from "./views/skills.component";
-import ExperienceView from "./views/experience.component";
-import NavbarOverlay from "./layout/navbar-overlay.component";
+import { useContext, useEffect, useState } from 'react';
+import { NavigationContext, Views } from './context/navigation.context';
+import TransitionLoading from './components/transition-loading/transition-loading.component';
+import { EmptyView } from './layout/empty-view.component';
+import { cn } from './utils/cn';
+import ContactView from './views/contact.component';
+import AboutView from './views/home.component';
+import ProjectsView from './views/projects.component';
+import SkillsView from './views/skills.component';
+import ExperienceView from './views/experience.component';
+import NavbarOverlay from './layout/navbar-overlay.component';
 import {
   faAt,
   faBriefcase,
   faListCheck,
   faStar,
   faUser,
-} from "@fortawesome/free-solid-svg-icons";
-import ViewLayout from "./layout/view-layout.component";
-import { ProjectListProvider } from "./context/project-list.context";
+} from '@fortawesome/free-solid-svg-icons';
+import ViewLayout from './layout/view-layout.component';
+import { ProjectListProvider } from './context/project-list.context';
 
 const getShouldOffset = () => {
-  return window.matchMedia("(min-width: 1024px)");
+  return window.matchMedia('(min-width: 1024px)');
 };
 
 export default function App() {
   const { currentView, isMoving, isGlobalView } = useContext(NavigationContext);
   const [isLargeScreen, setIsLargeScreen] = useState<boolean>(
-    getShouldOffset().matches
+    getShouldOffset().matches,
   );
 
   useEffect(() => {
     const handler = (e: MediaQueryListEvent) => {
       setIsLargeScreen(e.matches);
     };
-    getShouldOffset().addEventListener("change", handler);
+    getShouldOffset().addEventListener('change', handler);
 
     return () => {
-      getShouldOffset().removeEventListener("change", handler);
+      getShouldOffset().removeEventListener('change', handler);
     };
   }, []);
 
@@ -45,26 +45,31 @@ export default function App() {
       <TransitionLoading></TransitionLoading>
       <NavbarOverlay></NavbarOverlay>
 
-      <div className="flex h-screen w-screen bg-base-100 flex-1 main-background bg-center overflow-hidden duration-1000 bg-[length:3rem_3rem]">
+      <div
+        className={cn(
+          'flex h-screen w-screen bg-base-100 flex-1 main-background bg-center overflow-hidden duration-1000 bg-[length:3rem_3rem]',
+          isGlobalView && 'bg-[length:1.7rem_1.7rem]',
+        )}
+      >
         <div
           style={{
             transform: `translateX(${
               !isGlobalView
                 ? currentView.horizontalTranslation
                 : isLargeScreen
-                ? "calc(-100vw + 2.2rem)"
-                : "-100vw"
+                ? 'calc(-100vw + 2.2rem)'
+                : '-100vw'
             }) translateY(${
               !isGlobalView
                 ? currentView.verticalTranslation
                 : isLargeScreen
-                ? "-100vh"
-                : "calc(-100vh + 2rem)"
-            }) scale(${isGlobalView ? "25%" : "100%"})`,
+                ? '-100vh'
+                : 'calc(-100vh + 2rem)'
+            }) scale(${isGlobalView ? '25%' : '100%'})`,
           }}
           className={cn(
-            "grid grid-cols-3 min-h-[300vh] min-w-[300vw]",
-            isMoving && "duration-700"
+            'grid grid-cols-3 min-h-[300vh] min-w-[300vw]',
+            isMoving && 'duration-700',
           )}
         >
           <ViewLayout view={Views.SKILLS} icon={faStar}>
