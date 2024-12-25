7ed134fd729f5b60fd28ad23e350bc4eceea275e
Debbal Lotfi <debbal.lotfi.dev@gmail.com>
2024-12-25 21:00:13 +0100

    update

--- codegen.js
+++
@@ -1,8 +0,0 @@
-import fs from "node:fs";
-import openapiTS, { astToString } from "openapi-typescript";
-
-const ast = await openapiTS(new URL("http://localhost:8000/api/schema"));
-const contents = astToString(ast);
-
-// (optional) write to file
-fs.writeFileSync("./src/__generated__/schema.ts", contents);
